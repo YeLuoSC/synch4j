e83cddf7c42b428a36bd544e4feaecc8062ff357
YeLuoSC <101002552@qq.com>
2016-04-16 22:22:14 +0800

    update

--- src/main/java/com/synch4j/sqlgenerator/exp/impl/MysqlCommonExportSqlGenerator.java
+++ src/main/java/com/synch4j/sqlgenerator/exp/impl/MysqlCommonExportSqlGenerator.java
@@ -3,10 +3,16 @@ package com.synch4j.sqlgenerator.exp.impl;
 import java.util.List;
 import java.util.Map;
 
+import javax.annotation.Resource;
+
+import org.apache.log4j.Logger;
+import org.springframework.stereotype.Component;
+
 import com.synch4j.Synch2Context;
 import com.synch4j.exception.CallbackException;
 import com.synch4j.po.SynchPO;
 import com.synch4j.sqlgenerator.exp.IExportSqlGenerator;
+import com.synch4j.sqlgenerator.exp.dao.IExportSqlGeneratorMapper;
 import com.synch4j.synchenum.ExportMode;
 
 /**
@@ -16,8 +22,14 @@ import com.synch4j.synchenum.ExportMode;
  * @author xie
  * @date 2016-4-15 下午8:08:04
  */
+@Component
 public class MysqlCommonExportSqlGenerator implements IExportSqlGenerator{
 
+	Logger logger = Logger.getLogger(OracleCommonExportSqlGenerator.class);
+	
+	@Resource
+	private IExportSqlGeneratorMapper exportSqlGeneratorMapper;
+	
 	@Override
 	public Map<String, String> getExportSqlInfo(ExportMode mode,
 			SynchPO synchPO, List<Map<String, String>> tableColumns,
